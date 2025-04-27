6c281fb69eec830b95a0b94ecfbeb6d11a27f8e6
Alex Gomez <alexgofer290@gmail.com>
2025-04-26 20:02:39 -0600

    SimonCommit

---
+++ AdministracionDePersonal.Entities/Accion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionDePersonal.Entities
+{
+    public class Accion
+    {
+        public int IdAccion { get; set; }
+        public int? IdEmpleado { get; set; }
+        public int? IdJefatura { get; set; }
+        public DateTime? Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public string TipoAccion { get; set; }
+        public string Observaciones { get; set; }
+    }
+
+}
