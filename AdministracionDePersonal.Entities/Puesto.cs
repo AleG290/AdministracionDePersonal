3056c50703f8e72ecc099d8a0dacff328cfbf2a4
Alex Gomez <alexgofer290@gmail.com>
2025-04-26 20:13:19 -0600

    AlexCommit

---
+++ AdministracionDePersonal.Entities/Puesto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionDePersonal.Entities
+{
+    public class Puesto
+    {
+        public int IdPuesto { get; set; }
+        public string Nombre { get; set; }
+        public decimal? Salario { get; set; } 
+        public int? IdJefatura { get; set; }   
+    }
+
+}
---
+++ AdministracionDePersonal.Repository/DbConnectionFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionDePersonal.Repository
+{
+    public class DbConnectionFactory : IDbConnectionFactory
+    {
+        private readonly IConfiguration _configuration;
+
+        public DbConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new MySqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
